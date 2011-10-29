0b54a1746f4c44c524aa065a94004cbab4a6451a
David Dawson <david.dawson@dawsonsystems.com>
2011-10-29 16:36:32 +0100

    s

---
+++ src/gwt/uk/co/devooght/stock/client/view/StockEntryPoint.java
@@ -0,0 +1,19 @@
+package uk.co.devooght.stock.client.view;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+//import uk.co.devooght.stock.client.gin.StockGinjector;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class StockEntryPoint implements EntryPoint {
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+      //  StockGinjector ginjector = GWT.create(StockGinjector.class);
+        //RootLayoutPanel.get().add(ginjector.getHomePage());
+    }
+}
