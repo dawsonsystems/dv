68749f8043827329c4da977a634ddf8e20b70a4b
David Dawson <david.dawson@dawsonsystems.com>
2011-12-18 21:46:19 +0000

    asa

---
+++ src/gwt/uk/co/devooght/stock/client/view/maintabs/ProductTab.groovy
@@ -0,0 +1,11 @@
+package uk.co.devooght.stock.client.view.maintabs
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: david
+ * Date: 17/11/11
+ * Time: 15:31
+ * To change this template use File | Settings | File Templates.
+ */
+class ProductTab {
+}
